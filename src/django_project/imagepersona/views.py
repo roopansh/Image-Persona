1106fddf87600668ae37ba29bbb63f719e19b978
roopansh <roopansh.bansal@gmail.com>
2017-12-04 17:08:22 +0530

    Login and Registration Page
    Still need Better Validation

--- src/django_project/imagepersona/views.py
+++ src/django_project/imagepersona/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -17,8 +18,31 @@ def login_user(request):
                 login(request, user)
                 return HttpResponse("Login Successful!")
             else:
-                return render(request, 'imagepersona/login.html', {'error_message': 'Your account has been disabled'})
+                return render(request, 'imagepersona/login.html', {'login_error_message': 'Your account has been disabled'})
         else:
-            return render(request, 'imagepersona/login.html', {'error_message': 'Invalid login'})
+            return render(request, 'imagepersona/login.html', {'login_error_message': 'Invalid login'})
     return render(request, 'imagepersona/login.html')
 
+# User Log Out
+def logout_user(request):
+    logout(request)
+    return HttpResponse("Logged Out Successfully!")
+
+# Register New User
+def register_user(request):
+    if request.method == "POST":
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username, email, password)
+        user.last_name = lastname
+        user.first_name = firstname
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse("Registered and Logged In Successfully!")
+    return render(request, 'imagepersona/login.html')
