c698ab6e2891a262bf1a0de4feca946f93d57069
Mahim Goyal <mahim97@gmail.com>
2017-12-04 21:36:11 +0530

    Add Upload view and change some views in views.py

--- src/django_project/imagepersona/urls.py
+++ src/django_project/imagepersona/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^login/$', views.login_user, name='login'),
     url(r'^register/$', views.register_user, name='register'),
     url(r'^logout/$', views.logout_user, name='logout'),
+    url(r'^upload/$', views.upload, name='upload'),
+
 
 ]
