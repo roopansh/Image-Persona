cd95b27b680782611bd423deb67db91f5344d8cd
Mahim Goyal <mahim97@gmail.com>
2017-12-10 01:39:58 +0530

    Improve photo Gallery in UI

---
+++ src/django_project/imagepersona/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('imagepersona', '0020_forgotpassword'),
+        ('imagepersona', '0021_auto_20171209_1813'),
+    ]
+
+    operations = [
+    ]
